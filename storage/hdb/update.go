4fb514dfcf13f5732c1303360ef0b5dc0120e605
UNIVN <valuable.network.lz@gmail.com>
2025-07-01 01:20:40 +0800

    add update affected rows

--- storage/hdb/update.go
+++ storage/hdb/update.go
@@ -8,12 +8,17 @@ import (
 )
 
 func Update[T any](dbTx *gorm.DB, mut map[string]any, query any, args ...any) error {
+	_, err := UpdateX[T](dbTx, mut, query, args...)
+	return err
+}
+
+func UpdateX[T any](dbTx *gorm.DB, mut map[string]any, query any, args ...any) (int64, error) {
 	var m T
 	tx := dbTx.Model(&m).Where(query, args...).Updates(mut)
 	if tx.Error != nil {
-		hlog.Err("hdb.Update", zap.Any("mut", mut),
+		hlog.Err("hdb.UpdateX", zap.Any("mut", mut),
 			zap.Any("query", query), zap.Any("args", args), zap.Error(tx.Error))
-		return errors.New("db.update error:" + tx.Error.Error())
+		return 0, errors.New("db.update error:" + tx.Error.Error())
 	}
-	return nil
+	return tx.RowsAffected, nil
 }
