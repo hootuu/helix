b54bf8dc119593ed9505f1d9f2985f8ac558749c
UNIVN <valuable.network.lz@gmail.com>
2025-07-15 02:59:58 +0800

    add public producer

--- components/zplt/mq.go
+++ components/zplt/mq.go
@@ -1,20 +1,58 @@
 package zplt
 
 import (
+	"github.com/hootuu/helix/helix"
 	"github.com/hootuu/helix/unicom/hmq/hmq"
 	"github.com/hootuu/helix/unicom/hmq/hnsq"
+	"sync"
 )
 
 const (
-	helixMainMQ = "helix_main_nsq"
+	helixMainMQ         = "helix_main_nsq"
+	helixMainMqProducer = "helix_main_nsq_producer"
 )
 
 var gMQ *hmq.MQ
+var gMqProducer *hmq.Producer
+var gMqMu sync.Mutex
 
 func HelixMQ() *hmq.MQ {
 	return gMQ
 }
 
+func HelixMqProducer() (*hmq.Producer, error) {
+	if gMqProducer != nil {
+		return gMqProducer, nil
+	}
+	gMqMu.Lock()
+	defer gMqMu.Unlock()
+	if gMqProducer != nil {
+		return gMqProducer, nil
+	}
+	producer := HelixMQ().NewProducer()
+	err := HelixMQ().RegisterProducer(producer)
+	if err != nil {
+		return nil, err
+	}
+	gMqProducer = producer
+	return gMqProducer, nil
+}
+
+func HelixMqPublish(topic hmq.Topic, payload hmq.Payload) error {
+	producer, err := HelixMqProducer()
+	if err != nil {
+		return err
+	}
+	return producer.Publish(topic, payload)
+}
+
 func init() {
-	gMQ = hmq.NewMQ(helixMainMQ, hnsq.NewNsqMQ())
+	helix.AfterStartup(func() {
+		gMQ = hmq.NewMQ(helixMainMQ, hnsq.NewNsqMQ())
+		gMqProducer = gMQ.NewProducer()
+		err := gMQ.RegisterProducer(gMqProducer)
+		if err != nil {
+
+		}
+	})
 }
