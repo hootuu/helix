88af59e7c3bdcc1d90ac53b0876782ad6634ed86
UNIVN <valuable.network.lz@gmail.com>
2025-07-22 20:14:03 +0800

    fix redis set with json

--- storage/hrds/set.go
+++ storage/hrds/set.go
@@ -12,6 +12,24 @@ import (
 	"time"
 )
 
+func (cache *Cache) SetString(key string, val string, expiration time.Duration) error {
+	err := retry.Do(func() error {
+		cmd := cache.Redis().Set(context.Background(), key, val, expiration)
+		if err := cmd.Err(); err != nil {
+			return err
+		}
+		return nil
+	},
+		retry.Attempts(cast.ToUint(hcfg.GetInt("hrds."+cache.code+".retry.attempts", 3))),
+		retry.Delay(hcfg.GetDuration("hrds."+cache.code+".retry.delay", 200*time.Millisecond)),
+	)
+	if err != nil {
+		hlog.Err("hrds.Set", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
 func (cache *Cache) Set(key string, val interface{}, expiration time.Duration) error {
 	if val == nil {
 		return errors.New("val is nil")
