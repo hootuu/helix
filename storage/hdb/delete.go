e6e30f02280ef5b5cdcb9bf4c5647bb8376c595e
wnjie <13296719323@163.com>
2025-09-03 09:54:39 +0800

    add soft delete

--- storage/hdb/delete.go
+++ storage/hdb/delete.go
@@ -1,6 +1,7 @@
 package hdb
 
 import (
+	"errors"
 	"github.com/hootuu/hyle/hlog"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -8,9 +9,23 @@ import (
 
 func Delete[T any](dbTx *gorm.DB, cond ...any) error {
 	var m T
-	tx := dbTx.Unscoped().Delete(&m, cond)
+	tx := dbTx.Unscoped().Delete(&m, cond...)
 	if tx.Error != nil {
 		hlog.Err("hdb.Delete error", zap.Error(tx.Error))
+		return tx.Error
+	}
+	return nil
+}
+
+func DeleteLogic[T any](dbTx *gorm.DB, cond ...any) error {
+	var m T
+	if len(cond) == 0 {
+		return errors.New("SoftDelete called without condition")
+	}
+
+	tx := dbTx.Delete(&m, cond...)
+	if tx.Error != nil {
+		hlog.Err("hdb.SoftDelete error", zap.Error(tx.Error))
 		return tx.Error
 	}
 	return nil
